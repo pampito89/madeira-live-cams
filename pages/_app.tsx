a1127f2ad45e6eda9eae8c566c88f83aac596a33
Alex <pampito89@gmail.com>
2026-07-10 16:46:14 +0100

    Restore global Tailwind styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+// @ts-ignore: Allow importing global CSS without type declarations
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
