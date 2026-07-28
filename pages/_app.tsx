a291cde913c443d0e880ccc620d9c4aac137143b
Alex <pampito89@gmail.com>
2026-07-28 14:59:54 +0100

    Fix SEO canonical robots and sitemap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import type { AppProps } from 'next/app';
-// @ts-ignore: Allow importing global CSS without type declarations
-import '../styles/globals.css';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import '../styles/globals.css';
+
+const SITE_URL = 'https://madeiralivecams.com';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const pathname = router.asPath.split('?')[0];
+  const normalizedPath = pathname === '/' ? '/' : pathname.replace(/\/$/, '');
+  const canonicalUrl = `${SITE_URL}${normalizedPath}`;
+
   return (
     <>
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="canonical" href={canonicalUrl} />
+        <meta name="robots" content="index,follow" />
+        <meta property="og:site_name" content="Madeira Live Cams" />
       </Head>
 
       <Component {...pageProps} />
