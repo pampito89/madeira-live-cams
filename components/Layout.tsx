f6a7c597ddc64f2cd73da10ba256d6674b421127
Alex <pampito89@gmail.com>
2026-08-29 09:57:51 +0100

    Hide parties navigation link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -47,11 +47,6 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       icon: '☀',
     },
     {
-      href: '/parties',
-      label: locale === 'uk' ? 'Вечірки' : 'Parties',
-      icon: '🎉',
-    },
-    {
       href: '/about',
       label: messages.nav.about,
       icon: 'ⓘ',
